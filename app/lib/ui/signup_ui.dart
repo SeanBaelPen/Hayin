02560515455a9bdab31dd56474dfc3d020a06c2a
unknown <jelianfortunado01@gmail.com>
2023-06-09 23:13:51 +0800

    added the authentication for sign up

--- app/lib/ui/signup_ui.dart
+++ app/lib/ui/signup_ui.dart
@@ -1,13 +1,60 @@
+import 'package:app/ui/welcome_ui.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'login_ui.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class SignUpPage extends StatefulWidget {
-  const SignUpPage({super.key});
+  const SignUpPage({Key? key}) : super(key: key);
 
   @override
   State<SignUpPage> createState() => _SignUpPageState();
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  late bool _success;
+  late String _userEmail;
+  String? _passwordError;
+
+  void _register() async {
+    if (_passwordController.text != _confirmPasswordController.text) {
+      setState(() {
+        _passwordError = "Passwords do not match.";
+      });
+      return;
+    }
+
+    final User? user = (await _auth.createUserWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    ))
+        .user;
+
+    if (user != null) {
+      setState(() {
+        _success = true;
+        _userEmail = user.email!;
+        _passwordError = null; // Reset the password error message
+      });
+      _clearTextFields(); // Clear the text fields
+    } else {
+      setState(() {
+        _success = false;
+      });
+    }
+  }
+
+  void _clearTextFields() {
+    _emailController.clear();
+    _passwordController.clear();
+    _confirmPasswordController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,11 +69,22 @@ class _SignUpPageState extends State<SignUpPage> {
         child: Column(
           children: [
             Row(
-              children: const [
-                Icon(
-                  Icons.arrow_back_ios_rounded,
-                  size: 50,
-                  color: Colors.white,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (BuildContext context) {
+                        return const WelcomePage();
+                      }),
+                    );
+                    print('Icon btn is tapped');
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back_ios_rounded,
+                    size: 50,
+                    color: Colors.white,
+                  ),
                 ),
               ],
             ),
@@ -35,7 +93,7 @@ class _SignUpPageState extends State<SignUpPage> {
                 Container(
                   margin: EdgeInsets.only(left: 45, top: 20),
                   child: const Text(
-                    'Welcome \nBack',
+                    'Create \nAccount',
                     style: TextStyle(
                       fontSize: 40,
                       fontWeight: FontWeight.bold,
@@ -56,35 +114,76 @@ class _SignUpPageState extends State<SignUpPage> {
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(8),
                   ),
-                  // color: Colors.white,
                   child: Column(
                     children: [
                       TextField(
+                        controller: _emailController,
                         decoration: InputDecoration(
                           hintText: 'Enter your email',
                         ),
                       ),
                       TextField(
+                        controller: _passwordController,
                         decoration: InputDecoration(
                           hintText: 'Enter your password',
                         ),
+                        obscureText: true,
                       ),
                       TextField(
+                        controller: _confirmPasswordController,
                         decoration: InputDecoration(
                           hintText: 'Confirm your password',
                         ),
+                        obscureText: true,
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Image.asset(
-                            'assets/signUpButton.png',
-                            width: 200,
-                            height: 200,
+                          GestureDetector(
+                            onTap: () async {
+                              if (_passwordController.text !=
+                                  _confirmPasswordController.text) {
+                                setState(() {
+                                  _passwordError = "Passwords do not match.";
+                                });
+                                return;
+                              }
+                              _register();
+                            },
+                            child: Container(
+                              child: Image.asset(
+                                'assets/signUpButton.png',
+                                width: 200,
+                                height: 200,
+                              ),
+                            ),
                           ),
                         ],
                       ),
-                      Text('Forgot Password?'),
+                      if (_passwordError != null)
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16),
+                          child: Text(
+                            _passwordError!,
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ),
+                      //const Text('Already have an account? Login'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Already have an account?"),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const LoginPage()));
+                            },
+                            child: const Text("Login"),
+                          )
+                        ],
+                      )
                     ],
                   ),
                 ),
