ae5041eb8ce97a7c37b8c397dc941a4fc77ef024
SeanBaelPen <seanbaelpen22@gmail.com>
2023-06-20 18:46:45 +0800

    Changed the screen

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:app/ui/home_ui.dart';
-import 'package:app/ui/location_ui.dart';
+import 'package:app/ui/restaurant_details.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,8 +16,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const HomePage(),
+    return const MaterialApp(
+      home: RestaurantDetails(),
     );
   }
 }
