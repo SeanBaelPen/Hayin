b0e786226c4fe472af9c0f0cda8e7c04b7ac00a8
Edison Modesto <edisonmodesto22@gmail.com>
2023-07-07 15:09:45 +0800

    added cooldown

--- app/lib/ui/home_ui.dart
+++ app/lib/ui/home_ui.dart
@@ -10,6 +10,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../ViewModels/authViewModel.dart';
 import '../services/GeolocatorService.dart';
@@ -25,11 +26,92 @@ class HomePage extends ConsumerStatefulWidget {
 
 class _HomePageState extends ConsumerState<HomePage> {
   int _selectedIndex = 0;
-  bool isRedeemed = false;
 
   @override
   void initState() {
     super.initState();
+  }
+
+  Future<void> checkLoc(restauData, location) async {
+    await ref.read(locationProvider.notifier).getCurrentLocation();
+    double minDistance = double.infinity;
+
+    var closest;
+    for (var restaurant in restauData.docs) {
+      var distance = Geolocator.distanceBetween(
+          location!.latitude,
+          location.longitude,
+          restaurant.data()['destinationLocation'].latitude,
+          restaurant.data()['destinationLocation'].longitude);
+      if (distance < minDistance) {
+        minDistance = distance;
+        closest = restaurant;
+      }
+    }
+    if (minDistance <= 10) {
+      // ignore: use_build_context_synchronously
+
+      // ignore: use_build_context_synchronously
+      showDialog(
+          context: context,
+          builder: (builder) {
+            return Center(
+              child: Card(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  height: 210,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "You are near ${closest.data()['name']}",
+                          style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "+5 Points",
+                          style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            SharedPreferences prefs =
+                                await SharedPreferences.getInstance();
+                            Fluttertoast.showToast(msg: "Points Claimed");
+                            FirestoreService().claimPoints();
+                            await prefs.setString(
+                                'currentDateTime', DateTime.now().toString());
+                            Navigator.pop(builder);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(double.infinity, 50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: Text(
+                            "Claim",
+                            style: GoogleFonts.poppins(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          });
+    }
   }
 
   @override
@@ -94,90 +176,24 @@ class _HomePageState extends ConsumerState<HomePage> {
                     setState(() {
                       _selectedIndex = index;
                     });
-                    await ref
-                        .read(locationProvider.notifier)
-                        .getCurrentLocation();
-                    double minDistance = double.infinity;
 
-                    var closest;
-                    for (var restaurant in restauData.docs) {
-                      var distance = Geolocator.distanceBetween(
-                          location!.latitude,
-                          location.longitude,
-                          restaurant.data()['destinationLocation'].latitude,
-                          restaurant.data()['destinationLocation'].longitude);
-                      if (distance < minDistance) {
-                        minDistance = distance;
-                        closest = restaurant;
-                      }
-                    }
-                    if (minDistance <= 10) {
-                      // ignore: use_build_context_synchronously
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
 
-                      if (!isRedeemed) {
-                        // ignore: use_build_context_synchronously
-                        showDialog(
-                            context: context,
-                            builder: (builder) {
-                              return Center(
-                                child: Card(
-                                  child: SizedBox(
-                                    width:
-                                        MediaQuery.of(context).size.width * 0.9,
-                                    height: 210,
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 25, vertical: 20),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            "You are near ${closest.data()['name']}",
-                                            style: GoogleFonts.poppins(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          Text(
-                                            "+5 Points",
-                                            style: GoogleFonts.poppins(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          ElevatedButton(
-                                            onPressed: () {
-                                              Fluttertoast.showToast(
-                                                  msg: "Points Claimed");
-                                              FirestoreService().claimPoints();
-                                              isRedeemed = true;
-                                              Navigator.pop(builder);
-                                            },
-                                            style: ElevatedButton.styleFrom(
-                                              fixedSize: const Size(
-                                                  double.infinity, 50),
-                                              shape: RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(10),
-                                              ),
-                                            ),
-                                            child: Text(
-                                              "Claim Points",
-                                              style: GoogleFonts.poppins(
-                                                fontSize: 18,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            });
+                    String? storedDateTime = prefs.getString('currentDateTime');
+
+                    if (storedDateTime != null) {
+                      DateTime parsedDate = DateTime.parse(storedDateTime);
+
+                      if (DateTime.now().difference(parsedDate) >=
+                          Duration(days: 1)) {
+                        checkLoc(restauData, location);
+                      } else {
+                        Fluttertoast.showToast(
+                            msg: "You can only claim points once a day");
                       }
+                    } else {
+                      checkLoc(restauData, location);
                     }
                   },
                 ),
